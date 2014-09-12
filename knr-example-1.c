0189430dbf12b50035c658baa81ce4ca173271e0
Axel Ivanova <melnelen@gmail.com>
2014-09-12 21:58:43 +0300

    Create knr-example-1.c

---
+++ knr-example-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main(int argc, const char * argv[])
+{
+    int x, y;
+    x = 2;
+    y = 3;
+    while (x < y) {
+        printf("x < y\n");
+         
+        if (x == y) {
+            printf("x = y\n");
+        } else {
+            x++;
+        }
+    }
+    printf("x = %d\n",x);
+    return 0;
+}
